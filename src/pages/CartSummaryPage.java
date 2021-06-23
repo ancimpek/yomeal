314508c2344cc12033b1ede13602e6024235e3f4
ancimpek <anci.mpek@gmail.com>
2021-06-24 00:56:19 +0200

    Add CartSummaryPage

---
+++ src/pages/CartSummaryPage.java
@@ -0,0 +1,24 @@
+package pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class CartSummaryPage extends BasicPage{
+
+	public CartSummaryPage(WebDriver driver, WebDriverWait waiter, JavascriptExecutor js) {
+		super(driver, waiter, js);
+	}
+	
+	public WebElement getClearAllBtn() {
+		return this.driver.findElement(By.partialLinkText("Clear All"));
+	}
+
+	public void cartClearAll() {
+		this.getClearAllBtn().click();
+	}
+
+
+}
