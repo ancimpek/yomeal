07d9b607f32841b6cc0c1d5f3d9fc43419e3ccd2
ancimpek <anci.mpek@gmail.com>
2021-06-23 19:48:16 +0200

    Edit NotificationSystemPage

--- src/pages/NotificationSystemPage.java
+++ src/pages/NotificationSystemPage.java
@@ -7,23 +7,24 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
-public class NotificationSystemPage extends BasicPage{
+public class NotificationSystemPage extends BasicPage {
 
 	public NotificationSystemPage(WebDriver driver, WebDriverWait waiter, JavascriptExecutor js) {
 		super(driver, waiter, js);
 	}
 
 	public WebElement getMessageElement() {
-		return driver.findElement(By.xpath("//*[contains(@class, 'alert--success') or "
-				+ "contains(@class, 'alert--danger')][contains(@style,'display: block')]"));
+		return driver.findElement(By.xpath(
+				"//*[contains(@class, 'alert--success') or contains(@class, 'alert--danger')][contains(@style,'display: block')]"));
 	}
-	
+
 	public String getMesssageTxt() {
 		return this.getMessageElement().getText();
 	}
-	
-	public void waitForMsgDisappear() {
-		waiter.until(ExpectedConditions.attributeContains(
-				By.xpath(" //*[contains(@class, 'system_message')]"), "style", "display: none;"));
+
+	public void waitUntilMessageDisappears() {
+		WebDriverWait waiter = new WebDriverWait(driver, 30);
+		waiter.until(ExpectedConditions.attributeContains(this.getMessageElement(), "style",
+				"display: none;"));
 	}
 }
